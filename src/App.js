2a2bb8dc06e0eacf7be3db0b483379a34af6ebd2
David Ihl <daih1985@gmail.com>
2023-05-11 17:12:14 +0200

    Add switch to change attendance

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import {
   Button,
-  Checkbox,
-  FormControl,
+  IconButton,
+  Switch,
   TextField,
   Typography,
-  useScrollTrigger,
 } from '@mui/material';
 import Box from '@mui/material/Box';
 import { useEffect, useRef, useState } from 'react';
+import { uid } from 'uid';
 import styles from './App.module.scss';
 
 export default function App() {
@@ -26,15 +26,32 @@ export default function App() {
   }, [guests]);
 
   // Add user to array by clicking
-  function addGuest() {
-    const newGuest = [
-      { firstName: name, lastName: lastName, attending: false },
-    ];
+  const addGuest = () => {
+    const newGuest = {
+      uid: uid(),
+      firstName: name,
+      lastName: lastName,
+      attends: false,
+    };
     anotherGuest.current.focus();
     setGuests([...guests, newGuest]);
     setName('');
     setLastName('');
-  }
+  };
+
+  // Change attending
+  const handleAttending = (event, index) => {
+    const isAttending = [...guests];
+    isAttending[index].attends = !isAttending[index].attends;
+    setGuests(isAttending);
+  };
+
+  // Change attending
+  const deleteGuest = (event, index) => {
+    const isAttending = [...guests];
+    // concat array
+    setGuests(isAttending);
+  };
 
   // Add user to array by pressing enter
   const handleKeyDown = (event) => {
@@ -48,8 +65,27 @@ export default function App() {
       <Typography variant="h1" gutterBottom>
         Guest List
       </Typography>
-      <div data-test-id="guest" className={styles.guest}>
-        Guest List Placeholder
+      <div className={styles.guestList}>
+        {guests.map((guest, index) => {
+          return (
+            <div
+              key={`guest-${guest.uid}`}
+              data-test-id="guest"
+              className={styles.guest}
+            >
+              <div>
+                {guest.firstName} {guest.lastName}
+              </div>
+
+              <div className={styles.guestControl}>
+                <span>
+                  {guest.attends ? 'is attanding' : 'is not attending'}
+                </span>
+                <Switch onClick={(event) => handleAttending(event, index)} />
+              </div>
+            </div>
+          );
+        })}
       </div>
       <div id="form">
         <form
