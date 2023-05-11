bd569f7a07a28ecaa51b60e1ec1860c010c5bda8
David Ihl <daih1985@gmail.com>
2023-05-11 16:01:01 +0200

    Add input fields to array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import './App.css';
-import logo from './logo.svg';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  TextField,
+  Typography,
+  useScrollTrigger,
+} from '@mui/material';
+import Box from '@mui/material/Box';
+import { useEffect, useRef, useState } from 'react';
+import styles from './App.module.scss';
 
 export default function App() {
+  const anotherGuest = useRef(null);
+  const [guests, setGuests] = useState([]);
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  // Debug
+  useEffect(() => {
+    console.log(guests);
+  }, [guests]);
+
+  // Add user to array by clicking
+  function addGuest() {
+    const newGuest = [
+      { firstName: name, lastName: lastName, attending: false },
+    ];
+    anotherGuest.current.focus();
+    setGuests([...guests, newGuest]);
+    setName('');
+    setLastName('');
+  }
+
+  // Add user to array by pressing enter
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      addGuest();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={styles.container}>
+      <Typography variant="h1" gutterBottom>
+        Guest List
+      </Typography>
+      <div data-test-id="guest" className={styles.guest}>
+        Guest List Placeholder
+      </div>
+      <div id="form">
+        <form
+          className={styles.form}
+          onSubmit={(event) => event.preventDefault()}
         >
-          Learn React
-        </a>
-      </header>
+          <TextField
+            label="First name"
+            variant="filled"
+            value={name}
+            onChange={(event) => setName(event.currentTarget.value)}
+            inputRef={anotherGuest}
+          />
+          <TextField
+            label="Last name"
+            variant="filled"
+            value={lastName}
+            onChange={(event) => setLastName(event.currentTarget.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <Button variant="contained" size="large" onClick={() => addGuest()}>
+            Add guest
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
